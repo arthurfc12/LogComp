55db23fa9d7caeed5dc759bc51bd88183617ee7c
arthurfc12 <arthur0912fc@gmail.com>
2023-08-29 11:35:54 -0300

    ve se vai

--- main.py
+++ main.py
@@ -60,46 +60,48 @@ class Parser:
 
     @staticmethod
     def parseTerm():
+        result = 0
+        if(Parser.tokenizer.actual.type != "NUM"): #checa se eh numerico
+            raise ValueError
+        result = Parser.tokenizer.actual.value
         Parser.tokenizer.selectNext()
-        if(Parser.tokenizer.actual.type == "NUM"): #checa se eh numerico
-            result = Parser.tokenizer.actual.value
-            Parser.tokenizer.selectNext()
-            if(Parser.tokenizer.actual.type == "MULT" or Parser.tokenizer.actual.type == "DIV"):
-                while(Parser.tokenizer.actual.type == "MULT" or Parser.tokenizer.actual.type == "DIV"):
-                    if(Parser.tokenizer.actual.type == "MULT"): #metodo para mult
-                        Parser.tokenizer.selectNext()
-                        if(Parser.tokenizer.actual.type == "NUM"):
-                            result *=Parser.tokenizer.actual.value
-                        else:
-                            raise Exception("sequencia invalida (multiplicacao)")
-                    if(Parser.tokenizer.actual.type == "DIV"): #metodo para div
-                        Parser.tokenizer.selectNext()
-                        if(Parser.tokenizer.actual.type == "NUM"):
-                            result /= Parser.tokenizer.actual.value
-                        else:
-                            raise Exception("sequencia invalida (divisao)")
+        
+        while(Parser.tokenizer.actual.type == "MULT" or Parser.tokenizer.actual.type == "DIV") and Parser.tokenizer.actual.type != "EOF":
+                if(Parser.tokenizer.actual.type == "MULT"): #metodo para mult
+                    Parser.tokenizer.selectNext()
+                    if(Parser.tokenizer.actual.type == "NUM"):
+                        result *=Parser.tokenizer.actual.value
+                    else:
+                        raise Exception("sequencia invalida (multiplicacao)")
+                if(Parser.tokenizer.actual.type == "DIV"): #metodo para div
+                    Parser.tokenizer.selectNext()
+                    if(Parser.tokenizer.actual.type == "NUM"):
+                        result /= Parser.tokenizer.actual.value
+                    else:
+                        raise Exception("sequencia invalida (divisao)")
                         #pass
-                    Parser.tokenizer.selectNext() # vai pro prox digito
-                return result
-            else:
-                return result
-        else:
-            raise Exception("sequencia invalida")
+                Parser.tokenizer.selectNext() # vai pro prox digito
+        return result
+            
     
     def parseExpression():
         result = 0
         result += Parser.parseTerm()
-        while (Parser.tokenizer.actual.type == "PLUS" or Parser.tokenizer.actual.type == "MINUS") and Parser.tokenizer.actual.type != "EOF":      
-            if(Parser.tokenizer.actual.type == "PLUS" or Parser.tokenizer.actual.type == "MINUS"):
-                if(Parser.tokenizer.actual.type == "PLUS"):
-                    result+=Parser.parseTerm()
-                elif(Parser.tokenizer.actual.type == "MINUS"):
-                    result-=Parser.parseTerm()
-                elif(Parser.tokenizer.actual.type == "NUM"):
-                    raise Exception("sequencia invalida")
+        while(Parser.tokenizer.actual.type == "PLUS" or Parser.tokenizer.actual.type == "MINUS") and Parser.tokenizer.actual.type != "EOF":
+            if(Parser.tokenizer.actual.type == "PLUS"):
+                Parser.tokenizer.selectNext()
+                result+=Parser.parseTerm()
+            elif(Parser.tokenizer.actual.type == "MINUS"):
+                Parser.tokenizer.selectNext
+                result-=Parser.parseTerm()
+            elif(Parser.tokenizer.actual.type == "NUM"):
+                raise Exception("sequencia invalida")
             else:
-                return int(result)
-
+                raise ValueError
+        if Parser.tokenizer.actual.type != "EOF":
+            raise ValueError
+        
+        return result
     
     def code_cleanup(code):
         return(re.sub('/[*](.*?)[*]/',"", code))
@@ -107,7 +109,12 @@ class Parser:
     @staticmethod
     def run(code):
         Parser.tokenizer = Tokenizer(Parser.code_cleanup(code))
-        return(Parser.parseExpression())
+        result = Parser.parseExpression()
+        if(Parser.tokenizer.actual.type == "EOF"):
+            return result
+        else:
+            raise Exception("seq invalida")
+
         #pass
 
 if __name__ == "__main__":
